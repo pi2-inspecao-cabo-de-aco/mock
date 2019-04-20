6dc5fea53735f6218402aa00f332d4c1f8e6c990
Vitor Borges <vitorbertulucci@gmail.com>
2019-04-19 21:11:17 -0300

    Adding ftp server configuration

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import FtpSrv from 'ftp-srv'
+
+async function main () {
+  let server = new FtpSrv()
+
+  server.on('login', (data, resolve, reject) => {
+    console.log('Connection stablished.')
+    resolve()
+  })
+
+  server.listen()
+}
+
+main().then().catch(console.log)
