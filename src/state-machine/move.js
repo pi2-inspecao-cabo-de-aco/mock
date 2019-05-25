0b789da750385b8f6f9c478375f99d0f60bf42bd
Lucas Andrade <lucasandrad@yandex.com>
2019-05-25 00:37:48 -0300

    Start to add the move command #18

---
+++ src/state-machine/move.js
@@ -0,0 +1,41 @@
+import {
+  setState,
+  getState,
+  setCurrentInterval,
+  getCurrentInterval,
+  setCurrentAnalysisLocation,
+  setCurrentAnalysisDirection,
+} from './state'
+import { getAllowedCommands } from '../helpers/generics'
+
+async function moveRobot (command, ciclingStates) {
+  console.log('CHECANDO ESTADO DA MÁQUINA...')
+  let state = getState()
+  console.log(`Estado atual: "${state}". Comando(s) desejado(s): "${getAllowedCommands(state, ciclingStates).join('; ')}"`)
+  // flow que deve ter aqui:
+  // verificar se o estado é o paused.
+  // colocar estado como moving
+  // Mover o robo na direcao correta.
+  // rodar o goRobot apenas uma vez
+  // voltar para o estado de paused
+  if (command === 'move') {
+    if (state === 'pased') {
+      let interval = getCurrentInterval()
+      setCurrentInterval(clearInterval(interval))
+      setState('moving')
+      state = getState()
+      console.log('----------------------------------------')
+      console.log(`Máquina parada. Estado atual: "${state}"`)
+    } else if (state === 'moving') {
+      return { err: '' }
+    } else {
+      return { err: '' }
+    }
+  } else {
+    return { err: 'Comando ou estado não permitido.' }
+  }
+}
+
+export {
+  pauseRobot
+}
