4787861819b75d753c8449881a26ab673790f7fa
Lucas Andrade <lucasandrad@yandex.com>
2019-05-29 10:55:25 -0300

    Add a random value to get images from public folder #34

--- src/state-machine/init.js
+++ src/state-machine/init.js
@@ -6,7 +6,13 @@ import {
   setCurrentInterval,
   setLastImageCapture
 } from './state'
-import { sleep, getAllowedCommands } from '../helpers/generics'
+
+import {
+  sleep,
+  getAllowedCommands,
+  randomValue
+} from '../helpers/generics'
+
 import { endAnalisys } from '../helpers/end-cable'
 import fsx from 'fs-extra'
 import Path from 'path'
@@ -17,7 +23,8 @@ import { ftp } from '../ftp-client'
 let IMAGES_FOLDER = Path.resolve(__dirname, '../../public')
 
 async function getImages (direction, location) {
-  let imagePath = Path.resolve(IMAGES_FOLDER, `${location}.png`)
+  let imageNumber = randomValue([1, 2, 3, 4])
+  let imagePath = Path.resolve(IMAGES_FOLDER, `${imageNumber}.png`)
   let image = await fsx.readFile(imagePath)
   const zip = nodeZip()
   for (let i = 0; i < 4; i++) {
