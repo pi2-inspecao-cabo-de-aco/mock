36e63ecb804796ac4de0ef4e23cfca67edcdc5aa
Vitor Borges <vitorbertulucci@gmail.com>
2019-06-05 22:11:11 -0300

    Fixing error on generate zip without content on endCable

--- src/helpers/end-cable.js
+++ src/helpers/end-cable.js
@@ -2,18 +2,13 @@ import { getCurrentAnalysis } from '../state-machine/state'
 import { pauseRobot } from '../state-machine/pause'
 import { ciclingStates } from '../state-machine/index'
 import { ftp } from '../ftp-client'
+import { getImages } from '../state-machine/init'
 
-import Path from 'path'
 import fsx from 'fs-extra'
-
-let PUBLIC_FOLDER = Path.resolve(__dirname, '../../public')
 
 async function endAnalisys () {
   let location = getCurrentAnalysis().location
-  let time = Date.now()
-  let filename = `${time}-${location}-end.zip`
-  let zipPath = Path.join(PUBLIC_FOLDER, filename)
-  await fsx.writeFile(zipPath, null, 'binary')
+  let { zipPath, filename } = await getImages(undefined, location, true)
   let zipFile = await fsx.readFile(zipPath)
   ftp.put(zipFile, `public/${filename}`, (err) => {
     if (err) {
